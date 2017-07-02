6f2df5a60c56e0abd6c44f81d9dd5011a8df7103
Peter Joseph <peterjp96@gmail.com>
2017-07-02 09:38:13 +0530

    Initial Chat Box

---
+++ index.js
@@ -0,0 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var usernames={};
+var ms
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html')
+});
+
+io.on('connection', function(socket){
+
+  socket.on('adduser', function(username){
+	
+		socket.username = username;
+		ms="has connected";
+		io.emit('chat message',socket.username, ms)
+		usernames[username] = username;
+		 });
+		 
+  socket.on('chat message', function(msg){
+    io.emit('chat message',socket.username, msg)
+  });
+  
+ socket.on('disconnect', function(){
+    ms = "has disconnected"
+    io.emit('chat message',socket.username, ms)
+  });
+
+
+});
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
+    
+
